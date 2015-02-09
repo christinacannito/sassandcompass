bd5b0e1a0d1b2dddacd82e0ea6e540d554d85c09
Christina Cannito <christinacannito@gmail.com>
2015-02-09 14:42:26 -0500

    updated grunt file, changed index file, other.js and script.js

---
+++ config.rb
@@ -0,0 +1,4 @@
+css_dir = '_/css'
+sass_dir = '_/components/sass'
+javascript_dir = '_/javascript'
+output_style = :nested
--- gruntfile.js
+++ gruntfile.js
@@ -9,12 +9,26 @@ module.exports = function(grunt) {
 				} // files, first will be the destination of your finished javascript files, 2nd are the files that we want to process 
 			} // my_target 
 		}, // uglify (what we want grunt to take care of)
+		compass: {
+			dev: { 
+				options: {
+					config: 'config.rb'
+				} // options
+			} // dev 
+		}, // compass
 		watch: {
 			options: { livereload: true },
 			scripts: {
 				files: ['_/components/javascript/*.js'], //files we want to work with
 				tasks: ['uglify']  // what we want to do with the files
-			}	
+			},	// scripts
+			sass: { 
+				files: ['_/components/sass/*.scss'],
+				tasks: ['compass:dev']
+			}, // sass
+			html: {
+				files: ['*.html']
+			}
 		} // watch (list of files we want to watch)
 	}) // initconfig (what we want grunt to do)
 	grunt.registerTask('default', 'watch');
